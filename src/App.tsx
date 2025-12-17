e12a658299af230be2907d13acf47e1f7893c88f
davisdel <davisdeloach@gmail.com>
2025-12-17 17:22:23 -0500

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from '@/components/ui/toaster'
 import { Toaster as Sonner } from '@/components/ui/sonner'
 import { TooltipProvider } from '@/components/ui/tooltip'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import { AppTopbar } from './components/AppTopbar'
 import { Footer } from './components/Footer'
 import HomePage from './pages/Home'
@@ -21,7 +21,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <HashRouter>
         <Scroll />
         <div className='min-h-screen flex flex-col'>
           {/* Top Navigation */}
@@ -42,7 +42,7 @@ const App = () => (
           {/* Footer */}
           <Footer />
         </div>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 )
